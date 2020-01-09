b2dc11c6dc6c6e9671f84bcd3e3610aa276d1f5e
maxmyslivets <37196527+maxmyslivets@users.noreply.github.com>
2020-01-10 01:58:00 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+
+from kivymd.app import MDApp
+from kivy.config import Config
+from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+import time
+from kivy.properties import ObjectProperty
+from kivy.clock import Clock
+
+
+Config.set('graphics', 'fullscreen', 'auto')
+
+
+with open("myHookah.kv", encoding='utf8') as f:
+    drunk = Builder.load_string(f.read())
+
+
+class RootLayout(BoxLayout):
+
+    time_label = ObjectProperty()
+
+    def __init__(self, **kwargs):  
+        super(RootLayout, self).__init__(**kwargs) 
+
+        # функция вызова функции time_update() с интервалом в 1 секунду
+        Clock.schedule_interval(lambda dt: self.time_update(), .1)
+    
+    """
+    функция обновления времени в time_label
+    """
+    def time_update(self):
+
+        # забираем из модуля time значение времени "Wed Jan  8 13:27:34 2020"
+        # в time_label.text выводим [11:19] => "13:27:34"
+        self.time_label.text = time.ctime(time.time())[0:11] + '\n' + time.ctime(time.time())[11:19]
+    
+    def win_min(self):
+        
+        Config.set('graphics', 'window_state', 'minimized')
+
+
+class HookahSferaClubApp(MDApp):
+
+    def build(self):
+        
+        return RootLayout()
+
+
+if __name__ == '__main__':
+    HookahSferaClubApp().run()
