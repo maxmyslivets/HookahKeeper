78551fe303b54349eceb6d87a1d908702445a693
Max Myslivets <by.mikky1206@gmail.com>
2020-01-18 05:25:12 +0300

    Add function
    + закрытие модального окна при нажатии кнопки "добавить заказ"
    + передача времени заказа и номера стола в глобальные переменные

--- main.py
+++ main.py
@@ -46,6 +46,7 @@ class AddHokahWindow(BoxLayout):
 class ClassicHookah(BoxLayout):
     
     classic_time = ObjectProperty()
+    classic_stol_number = ObjectProperty()
 
     def __init__(self, **kwargs):  
         super(ClassicHookah, self).__init__(**kwargs)
@@ -80,34 +81,52 @@ class HookahSferaClubApp(MDApp):
         Config.set('graphics', 'window_state', 'minimized')
 
     def add_hookah_window(self):
+        global add_win
         add_win = ModalView(size_hint=(None, None), size=(340, 400))
         add_win.add_widget(AddHokahWindow())
         add_win.open()
     
     def classic_hookah(self):
+        global add_1
         add_1 = ModalView(size_hint=(None, None), size=(340, 400))
         add_1.add_widget(ClassicHookah())
         add_1.open()
+        add_win.dismiss()
+        
+
+    def stol_data(self, stol_number, time):
+        global stol_number_add_hookah
+        global time_add_hookah
+        stol_number_add_hookah = stol_number
+        time_add_hookah = time
+
+        print(stol_number_add_hookah, time_add_hookah)
+
+        add_1.dismiss()
     
     def additive_hookah(self):
         add_2 = ModalView(size_hint=(None, None), size=(340, 400))
         add_2.add_widget(AdditiveHookah())
         add_2.open()
+        add_win.dismiss()
     
     def fruit_hookah(self):
         add_3 = ModalView(size_hint=(None, None), size=(340, 400))
         add_3.add_widget(FruitHookah())
         add_3.open()
+        add_win.dismiss()
     
     def mix_hookah(self):
         add_4 = ModalView(size_hint=(None, None), size=(340, 400))
         add_4.add_widget(MixHookah())
         add_4.open()
+        add_win.dismiss()
     
     def replacement_hookah(self):
         add_5 = ModalView(size_hint=(None, None), size=(340, 400))
         add_5.add_widget(ReplacementHookah())
         add_5.open()
+        add_win.dismiss()
     
     def global_value_number_stol(self, data):
         global stol_number 
