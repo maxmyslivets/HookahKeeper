6a2452d5f7e167cb2ccc585ba67cd2ca02b34f8c
maxmyslivets <37196527+maxmyslivets@users.noreply.github.com>
2020-03-16 02:02:22 +0300

    add DataScreen
    Добавлен экран просмотра базы данных за выброный промежуток времени. В него добавлены кнопки выбора даты.

--- Lite-version/main.py
+++ Lite-version/main.py
@@ -2,19 +2,20 @@ from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.clock import Clock
+from datetime import datetime
 from time_format import time_edit_1, time_edit_2, time_edit_3, time_edit_4
 from kivymd.uix.list import OneLineAvatarIconListItem
 from kivymd.uix.list import IRightBodyTouch, OneLineAvatarIconListItem
 from kivymd.uix.selectioncontrol import MDCheckbox
-#from kivymd.uix.boxlayout import MDBoxLayout
-from kivy.uix.boxlayout import BoxLayout
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.picker import MDDatePicker
 
 
 class OrderListItem(OneLineAvatarIconListItem):
     '''Custom list item.'''
 
 
-class Container(IRightBodyTouch, BoxLayout):
+class Container(IRightBodyTouch, MDBoxLayout):
     adaptive_width = True
 
 
@@ -50,17 +51,40 @@ class Home(Screen):
                 stat.write(time_edit_1()+' '+time_edit_2()[1:-3]+' '+table+' '+additive+'\n')
 
         self.ids.mdlist.add_widget(order)
+    
+
+class Data(Screen):
+
+    with open("data.kv", encoding='utf8') as DataKV:
+        Builder.load_string(DataKV.read())
+
+    min_data = None
+
+    def show_date_picker_1(self):
+        MDDatePicker(self.set_previous_date_1).open()
+
+    def set_previous_date_1(self, date_obj):
+        date_obj = str(date_obj).split('-')
+        self.ids.date_picker_label_1.text = str(date_obj[2])+'.'+str(date_obj[1])+'.'+str(date_obj[0])[2:]
+    
+    def show_date_picker_2(self):
+        MDDatePicker(self.set_previous_date_2).open()
+
+    def set_previous_date_2(self, date_obj):
+        date_obj = str(date_obj).split('-')
+        print(date_obj)
+        self.ids.date_picker_label_2.text = str(date_obj[2])+'.'+str(date_obj[1])+'.'+str(date_obj[0])[2:]
 
 
 class HookahKeeperApp(MDApp):
 
     def build(self):
 
-        #self.theme_cls.primary_palette = "Blue"
         self.theme_cls.theme_style = "Light"
 
         sm = ScreenManager()
         sm.add_widget(Home(name='HomeScreen'))
+        sm.add_widget(Data(name='DataScreen'))
 
         return sm
 
